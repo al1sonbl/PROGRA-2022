a1709dc7a993a76bc52c8c5433de011a8396eeee
al1sonbl <109998483+al1sonbl@users.noreply.github.com>
2022-07-26 13:49:29 -0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yoconda
+{
+    public partial class Form1 : Form
+    {
+        static string conex = "server = DESKTOP-IQQ86GU; database = Yoconda; integrated security =true";
+        SqlConnection conexion = new SqlConnection(conex);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlCommand cm = new SqlCommand("Select nombre_Producto from TProducto", conexion);
+            conexion.Open();
+            SqlDataReader dr = cm.ExecuteReader();
+            while (dr.Read())
+            {
+                cmbx1.Items.Add(dr.GetString(0));
+            }
+            conexion.Close();
+
+            SqlCommand cm1 = new SqlCommand("Select Cantidad from TCantidad", conexion);
+            conexion.Open();
+            SqlDataReader dr1 = cm1.ExecuteReader();
+            while (dr1.Read())
+            {
+                comboBox1.Items.Add(dr1.GetInt32(0));
+            }
+            conexion.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+            MessageBox.Show("si");
+            conexion.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbx1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btn1_Click_1(object sender, EventArgs e)
+        {
+            conexion.Open();
+            string cadena = "Select nombre_Producto from TProducto where nombre_Producto  ='" + cmbx1.Text + "'" + "Select Cantidad2 From TProducto where Cantidad2 = '"+comboBox1.Text+"'";5
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            SqlDataAdapter adpt = new SqlDataAdapter(comando);
+            DataTable tabla = new DataTable();
+            adpt.Fill(tabla);
+            dataGridView1.DataSource = tabla;
+            conexion.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+    }
+
